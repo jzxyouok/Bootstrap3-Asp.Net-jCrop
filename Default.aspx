f33d2d600aa798721bbd40fdb30e621176f2ecc8
Ravi Mallya <ravimallya@gmail.com>
2014-07-04 14:36:07 +0530

    Initial Upload
    Initial Upload to the Repository

---
+++ Default.aspx
@@ -0,0 +1,147 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/modernizr-2.7.2.js"></script>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/bootstrap-theme.min.css" rel="stylesheet" />
+    <link href="Content/font-awesome.min.css" rel="stylesheet" />
+    <link href="Content/jquery.Jcrop.min.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="Default.aspx">jCrop ASP.Net</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret">
+                            </span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret">
+                            </span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <div class="container">
+            <asp:Image ID="imgProfile" runat="server" CssClass="img-responsive" />
+            <br />
+            <asp:FileUpload ID="fupProfileImg" runat="server" />
+            <br />
+            <asp:Button ID="btnProfileImage" runat="server" Text="Upload" CssClass="btn btn-primary"
+                OnClick="btnProfileImage_Click" />
+            <br />
+            <asp:Label ID="lblError" runat="server" Text="Label" Visible="false"></asp:Label>
+        </div>
+        <div class="modal fade" id="mdlImageCrop" tabindex="-1" role="dialog" aria-labelledby="mdlImageLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="mdlImageCropLabel">Crop Image</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            <%--<asp:Label ID="Label13" runat="server" Text="Please Upload the image of 1094x376 for Better Showing" Visible="false" />--%>
+                            <asp:Image ID="imgImageCrop" runat="server" />
+                            <br />
+                            <asp:HiddenField ID="hdnW" runat="server" />
+                            <asp:HiddenField ID="hdnH" runat="server" />
+                            <asp:HiddenField ID="hdnX" runat="server" />
+                            <asp:HiddenField ID="hdnY" runat="server" />
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <asp:Button ID="butImageCrop" runat="server" Text="Crop" CssClass="btn btn-primary"
+                            OnClick="butImageCrop_Click" />
+                        <asp:Button ID="butImageCropClose" runat="server" Text="Close" CssClass="btn btn-default" />
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <script src="Scripts/jquery-1.9.0.min.js"></script>
+        <script src="Scripts/bootstrap.min.js"></script>
+        <script src="Scripts/jquery.color.js"></script>
+        <script src="Scripts/jquery.Jcrop.min.js"></script>
+        <script type="text/javascript">
+            var ImagejcropValues = [
+            <asp:Repeater ID="rptImageJcropValues" runat="server">
+            <ItemTemplate>
+                     {
+                         "ImageminSizeWidth":'<%# Eval("ImageminSizeWidth") %>',
+                       "ImageminSizeHeight":'<%# Eval("ImageminSizeHeight") %>',
+                       "ImagemaxSizeWidth":'<%# Eval("ImagemaxSizeWidth") %>',
+                       "ImagemaxSizeHeight":'<%# Eval("ImagemaxSizeHeight") %>',
+                       "RatioIs":'<%# Eval("RatioIs") %>',
+                       "RatioTo":'<%# Eval("RatioTo") %>'
+                   }
+    </ItemTemplate>
+    <SeparatorTemplate>
+        ,
+    </SeparatorTemplate>
+    </asp:Repeater>
+          ];
+        
+          jQuery(document).ready(function () {
+              jQuery('#imgImageCrop').Jcrop({
+                  aspectRatio: ImagejcropValues[0].RatioIs / ImagejcropValues[0].RatioTo,
+                  minSize: [ImagejcropValues[0].ImageminSizeWidth, ImagejcropValues[0].ImageminSizeHeight],
+                  // maxSize: [jcropValues[0].maxSizeWidth, jcropValues[0].maxSizeHeight],
+                  // boxWidth: 270, boxHeight: 70,
+                  setSelect:[10,10,50,50],
+                  onSelect: storeImageCoords
+              });
+          });
+          function storeImageCoords(c) {
+              jQuery('#hdnX').val(c.x);
+              jQuery('#hdnY').val(c.y);
+              jQuery('#hdnW').val(c.w);
+              jQuery('#hdnH').val(c.h);
+          };
+        </script>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using SD = System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            SetImageJavascriptValues(200, 200, 0, 0, 1m, 1m);
+            imgProfile.ImageUrl = "Content/Images/not_available.jpg";
+        }
+    }
+    protected void btnProfileImage_Click(object sender, EventArgs e)
+    {
+        if (fupProfileImg.HasFile)
+        {
+            String[] allowedExtensions = { ".png", ".jpeg", ".jpg", ".gif" };
+            if (allowedExtensions.Contains(Path.GetExtension(fupProfileImg.FileName).ToLower()))
+            {
+                // this code will create a dynamic folder named "Images" in solution explorer...
+                Directory.CreateDirectory(Server.MapPath("TempImages"));
+                string UploadedImg = "TempImages/" + "Photo" + '-' + DateTime.Now.Ticks.ToString() + Path.GetExtension(fupProfileImg.PostedFile.FileName);
+                Session["ImgName"] = UploadedImg;
+                Stream strmImage = fupProfileImg.PostedFile.InputStream;
+                fupProfileImg.SaveAs(Server.MapPath(UploadedImg));
+                //Call resize start
+                string targetPath = Server.MapPath(UploadedImg);
+                var targetFile = targetPath;
+                GenerateThumbnails(strmImage, targetFile);
+                //Call resize end
+                imgImageCrop.ImageUrl = Session["ImgName"].ToString();
+                string scriptstring = "<script>$(function () {$('#mdlImageCrop').modal({show:true,keyboard:false,backdrop:'static'});});</script>";
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "", scriptstring, false);
+            }
+            else
+            {
+                lblError.Text = "Cannot accept files of this type.";
+                lblError.Visible = true;
+            }
+        }
+        else
+        {
+            lblError.Text = "Please select a file to upload.";
+            lblError.Visible = true;
+        }
+    }
+    protected void butImageCrop_Click(object sender, EventArgs e)
+    {
+        String path = HttpContext.Current.Request.PhysicalApplicationPath + Session["ImgName"].ToString(); 
+        // get the path of the image to crop
+        string ImageName = Session["ImgName"].ToString();
+        string abc = Math.Round(decimal.Parse(hdnW.Value)).ToString();
+        int w = Convert.ToInt32(Math.Round(decimal.Parse(hdnW.Value)));      // these are the 4 co-ordinates of the cropped image that we are storing
+        int h = Convert.ToInt32(Math.Round(decimal.Parse(hdnH.Value)));
+        int x = Convert.ToInt32(Math.Round(decimal.Parse(hdnX.Value)));
+        int y = Convert.ToInt32(Math.Round(decimal.Parse(hdnY.Value)));
+        //  Response.Write("w = " + w + " h= " + h + " x= " + x + " y=" + y);
+        //return;  //this is set by me to write the co-ordinates on the screen...
+        byte[] CropImage = Crop(path, w, h, x, y);
+        using (MemoryStream ms = new MemoryStream(CropImage, 0, CropImage.Length))
+        {
+            ms.Write(CropImage, 0, CropImage.Length);
+            using (SD.Image CroppedImage = SD.Image.FromStream(ms, true))
+            {
+                using (var image = System.Drawing.Image.FromStream(ms))
+                {
+                    var newWidth = image.Width;
+                    var newHeight = image.Height;
+                    
+                    var thumbnailImg = new Bitmap(newWidth, newHeight);
+                    var thumbGraph = Graphics.FromImage(thumbnailImg);
+                    thumbGraph.CompositingQuality = CompositingQuality.HighQuality;
+                    thumbGraph.SmoothingMode = SmoothingMode.HighQuality;
+                    thumbGraph.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    var imageRectangle = new Rectangle(0, 0, newWidth, newHeight);
+                    thumbGraph.DrawImage(image, imageRectangle);
+                    string SaveTo = HttpContext.Current.Request.PhysicalApplicationPath + Session["ImgName"].ToString().Replace("TempImages", "UploadedImages");
+                    thumbnailImg.Save(SaveTo, image.RawFormat);
+                   
+                        imgProfile.ImageUrl = Session["ImgName"].ToString().Replace("TempImages", "UploadedImages");
+                    
+                    if (File.Exists(path))
+                    {
+                        Session.Remove("ImgName");
+                        File.Delete(path);
+                    }
+                }
+            }
+        }
+    }
+    static byte[] Crop(string Img, int Width, int Height, int X, int Y)
+    {
+        try
+        {
+            using (SD.Image OriginalImage = SD.Image.FromFile(Img))
+            {
+                using (SD.Bitmap bmp = new SD.Bitmap(Width, Height))
+                {
+                    bmp.SetResolution(OriginalImage.HorizontalResolution, OriginalImage.VerticalResolution);
+                    using (SD.Graphics Graphic = SD.Graphics.FromImage(bmp))
+                    {
+                        Graphic.SmoothingMode = SmoothingMode.AntiAlias;
+                        Graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        Graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        Graphic.DrawImage(OriginalImage, new SD.Rectangle(0, 0, Width, Height), X, Y, Width, Height, SD.GraphicsUnit.Pixel);
+                        MemoryStream ms = new MemoryStream();
+                        bmp.Save(ms, OriginalImage.RawFormat);
+                        return ms.GetBuffer();
+                    }
+                }
+            }
+        }
+        catch (Exception Ex)
+        {
+            throw (Ex);
+        }
+    }
+   
+    private void SetImageJavascriptValues(int minWidth, int minHeight, int maxWidth, int maxHeight, decimal ratioIs, decimal ratioTo)
+    {
+        DataTable dt = new DataTable("ImageJcropValueTable");
+        dt.Columns.Add(new DataColumn("ImageminSizeWidth", typeof(int)));
+        dt.Columns.Add(new DataColumn("ImageminSizeHeight", typeof(int)));
+        dt.Columns.Add(new DataColumn("ImagemaxSizeWidth", typeof(int)));
+        dt.Columns.Add(new DataColumn("ImagemaxSizeHeight", typeof(int)));
+        dt.Columns.Add(new DataColumn("RatioIs", typeof(int)));
+        dt.Columns.Add(new DataColumn("RatioTo", typeof(int)));
+        DataRow dr = dt.NewRow();
+        dr["ImageminSizeWidth"] = minWidth;
+        dr["ImageminSizeHeight"] = minHeight;
+        dr["ImagemaxSizeWidth"] = maxWidth;
+        dr["ImagemaxSizeHeight"] = maxHeight;
+        dr["RatioIs"] = ratioIs;
+        dr["RatioTo"] = ratioTo;
+        dt.Rows.Add(dr);
+        rptImageJcropValues.DataSource = dt;
+        rptImageJcropValues.DataBind();
+    }
+    private void GenerateThumbnails(Stream sourcePath, string targetPath)
+    {
+        using (var image = SD.Image.FromStream(sourcePath))
+        {
+            //Default Width and Height for Jcrop Image
+            var newWidth = image.Width;//(int)(image.Width * scaleFactor);//double scaleFactor,
+            float newHeight = image.Height;//(int)(image.Height * scaleFactor);
+
+            if(image.Width>560)
+            {
+                newWidth = 560;
+                float aspectRatio = (float)image.Width / (float)image.Height;
+                newHeight = newWidth / aspectRatio;
+            }
+
+            var thumbnailImg = new Bitmap(newWidth, (int)Math.Ceiling(newHeight));
+            var thumbGraph = Graphics.FromImage(thumbnailImg);
+            thumbGraph.CompositingQuality = CompositingQuality.HighQuality;
+            thumbGraph.SmoothingMode = SmoothingMode.HighQuality;
+            thumbGraph.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            var imageRectangle = new Rectangle(0, 0, newWidth, (int)Math.Ceiling(newHeight));
+            thumbGraph.DrawImage(image, imageRectangle);
+            thumbnailImg.Save(targetPath, image.RawFormat);
+        }
+    }
+}
